282d51c3c2a5d61ff233aeb1d864ca8a6c07a9b3
Kalana Ratnayake <kalanaratnayake95@gmail.com>
2024-07-11 23:26:11 +1000

    minor fix

--- docker/workspace_entrypoint.sh
+++ docker/workspace_entrypoint.sh
@@ -4,6 +4,8 @@ set -e
 # start udev
 /lib/systemd/systemd-udevd --daemon
 
+echo 128 > /sys/module/usbcore/parameters/usbfs_memory_mb
+
 # setup ros2 environment
 source "/opt/ros/$ROS_DISTRO/setup.bash"
 source "$WORKSPACE_ROOT/install/setup.bash"
